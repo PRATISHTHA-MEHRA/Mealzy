a97a24bba746e8182b6e0ec0e4b478fc8b73ca0b
Pratishtha Mehra <pratishtha2021@gmail.com>
2026-07-06 17:40:59 +0530

    list-items01

---
+++ admin/src/components/ListItems/ListItems.jsx
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { FiTrash2, FiStar, FiHeart } from 'react-icons/fi';
+import AdminNavbar from '../Navbar/Navbar';
+import { styles } from '../../assets/dummyadmin';
+
