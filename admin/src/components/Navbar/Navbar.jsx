6b90fbfe7cf4011377dfc210b152ced4a4e0d83e
Pratishtha Mehra <pratishtha2021@gmail.com>
2026-07-08 16:22:07 +0530

    navbar01

---
+++ admin/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+// src/components/AdminNavbar.jsx
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import {
+  FiMenu,
+  FiX
+} from 'react-icons/fi';
+import { GiChefToque } from 'react-icons/gi';
+import { styles, navLinks } from '../../assets/dummyadmin';
