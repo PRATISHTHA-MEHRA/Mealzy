b09175f188a18e2aca46ba70ada8273ec0867045
Pratishtha Mehra <pratishtha2021@gmail.com>
2026-08-20 00:19:04 +0530

    changes in usermodal

--- backend/modals/userModal.js
+++ backend/modals/userModal.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
-const userSchema= new mongoose.Schema(
+const userSchema = new mongoose.Schema(
     {
-        username: {type:String, required:true},
-        email:{type: String, required: true, unique: true},
-        password:{type:String,required:true},
+        username: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        role: { type: String, enum: ['customer', 'admin'], default: 'customer' },
     }
 )
 
-const UserModel = mongoose.model.user || mongoose.model('user',userSchema)
+const UserModel = mongoose.model.user || mongoose.model('user', userSchema)
 
 export default UserModel;
